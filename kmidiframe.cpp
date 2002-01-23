ffe593703e2ae95a7bf09731ad8d599219276352
mhunter <mhunter@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2002-01-23 02:39:30 +0000

    CVS_SILENT Corrected typographical errors
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/kdemultimedia/kmidi@133245 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kmidiframe.cpp
+++ kmidiframe.cpp
@@ -171,7 +171,7 @@ KMidiFrame::KMidiFrame( const char *name ) :
 				"through the ends of their<br>\n"
 				"patches (which may cause some<br>\n"
 				"clicking). The wet setting makes<br>\n"
-				"notes continues to the ends of<br>\n"
+				"notes continue to the ends of<br>\n"
 				"their volume envelopes.") );
     reverb_level = new QPopupMenu();
     Q_CHECK_PTR( reverb_level );
